f41e5fed66119dbb289b88f84be879da5a5ceefe
Ryll974 <jullien.patrick@gmail.com>
2022-01-11 10:06:51 +0400

    commit 8 : ajout de la fonction Random sur les faces des Dés et implémentation de celle-ci dans les boutons -- passage en stateful

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.amber,
         appBar: AppBar(
           centerTitle: true,
           title: Text(
-            "Dédé",
+            "il faut gratter Dédé",
             style: TextStyle(
               color: Colors.amber,
               fontSize: 30.0,
@@ -17,14 +18,21 @@ void main() {
           ),
           backgroundColor: Colors.black,
         ),
-        body: DicePage (),
+        body: DicePage(),
       ),
     ),
   );
 }
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
 
-class DicePage extends StatelessWidget {
-  var leftDiceNumber = 5;
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = Random().nextInt(6) + 1;
+  int rightDiceNumber = Random().nextInt(6) + 1;
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -39,23 +47,27 @@ class DicePage extends StatelessWidget {
                     width: 200,
                     image: AssetImage("images/dice$leftDiceNumber.png"),
                   ),
-                  onPressed:() {
-                    print ("test dé 1");
+                  onPressed: () {
+                    setState ((){
+                      leftDiceNumber = Random().nextInt(6) + 1;
+                    });
+                    print("test dé 1");
                   },
-                )
-            ),
+                )),
             Expanded(
                 child: TextButton(
                   child: Image(
                     height: 200,
                     width: 200,
-                    image: AssetImage("images/dice6.png"),
+                    image: AssetImage("images/dice$rightDiceNumber.png"),
                   ),
-                  onPressed:() {
-                    print ("test dé 2");
+                  onPressed: () {
+                    setState ((){
+                      rightDiceNumber = Random().nextInt(6) + 1;
+                    });
+                    print("test dé 2");
                   },
-                )
-            ),
+                )),
           ],
         ),
       ),
